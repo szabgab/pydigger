2edd386ee0571d8f9c83152481c0d23dea1cdee8
Gabor Szabo <szabgab@gmail.com>
2013-01-11 10:50:20 +0200

    a bit more parsing

--- bin/parse.py
+++ bin/parse.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
 
-import tokenize
+#import tokenize
+from tokenize import *
 import sys
 
 def process(file):
   print 'Processing ' + file
-  fh = open(file, 'r')
-  g = tokenize.generate_tokens(fh)
-  print g
-  #for t, _, _, _, _ in g:
-  #  print t
-  #  pass
+  fh = open(file)
+  g = generate_tokens(fh.readline)
+  #print g
+  tree = []
+  for toknum, tokval, tokstart, tokend, tokline in g:
+    # 1  NAME  (from, tokenize, import, def, process,...)
+    # 4  NEWLINE
 
+    # added by the tokenizer
+    # 53 COMMENT comments starting with # and sh-bang?
+    # 54 NL
+
+    # 55 N_TOKENS  (if I understand this is the non-token, that is the number above the largest one in use)
+    print toknum, tokval
+    #print tokval
+    #if tokval == 'import':
+    #    print toknum
 
 
 def main():
