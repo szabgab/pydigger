c77bcdaa158f7734d9126161d674aac98a36eaa3
Gabor Szabo <gabor@szabgab.com>
2014-07-06 10:41:00 +0300

    download zip file

--- pydigger/implementation.py
+++ pydigger/implementation.py
@@ -1,10 +1,17 @@
 from __future__ import print_function, division
-import urllib2, feedparser, re, json
+import urllib, urllib2, feedparser, re, json, os
+from pymongo import MongoClient
 
 #import pkg_resources
 
+root = 'www';
+
 rss_feed = 'https://pypi.python.org/pypi?%3Aaction=rss'
 def run():
+	mongo_client = MongoClient('localhost', 27017)
+	mongo_db = mongo_client.pydigger
+	packages = mongo_db.packages
+
 	w = urllib2.urlopen(rss_feed)
 	rss = w.read()
 	#print(rss);
@@ -32,18 +39,53 @@ def run():
 
 		package = match.group(1)
 		version = match.group(2)
-		url = 'http://pypi.python.org/pypi/{}/{}/json'.format(package, version)
-		#print(url)
-		w = urllib2.urlopen(url)
-		json_string = w.read()
-		#print(json_string)
-		data = json.loads(json_string)
-		print(data['releases'][version][0]['url']);
+
+		data = packages.find_one({'package' : package, 'version' : version });
+		if data and data['status'] != 'waiting':
+			print("LOG: package {} version {} are already in the database".format(package, version))
+			continue
+
+		if not data:
+			data = {
+				'package' : package,
+				'version' : version,
+				'status'  : 'waiting'
+			}
+			packages.insert(data)
+			print("LOG: adding package {} version {} to the database ".format(package, version))
+			data = packages.find_one({'package' : package, 'version' : version });
+
+		#print(data)
+		if 'zip_url' not in data:
+			url = 'http://pypi.python.org/pypi/{}/{}/json'.format(package, version)
+			w = urllib2.urlopen(url)
+			json_string = w.read()
+			#print(json_string)
+			package_info = json.loads(json_string)
+			zip_url = package_info['releases'][version][0]['url']
+			if zip_url:
+				data['zip_url'] = zip_url
+				print("LOG: saving zip_url {}".format(data['zip_url']))
+				packages.save(data)
+			else:
+				print("LOG: zip_url missing from json for package {} version {}".format(package, version))
 
 		# It seems that a package might be already included in the RSS feed even before it was indexed by Pypi
 		# and so it does not yet have the details in the 'releases' and in the 'urls'
 		# we might need to take this in account when processng packages
 		# or maybe if there are multiple version numbers in the 'releases' ?
+
+		# remove the URL from the beginning of the zip_url and add it to the path to 'root'
+		m = re.search(r'^https://pypi.python.org/(.*)', data['zip_url'])
+		if not m:
+			print("ERROR: zi_url prefix does not match {}".format(data['zip_url']))
+			continue
+		local_path = root + '/' + m.group(1)
+		if not os.path.exists(os.path.dirname(local_path)):
+			print("LOG: creating dir {}".format(os.path.dirname(local_path)))
+			os.makedirs(os.path.dirname(local_path))
+		print("LOG: downloading {} to {}".format(data['zip_url'], local_path))
+		urllib.urlretrieve(data['zip_url'], local_path)
 
 		break
 	## fetch the rss feed
@@ -57,23 +99,4 @@ def run():
 	# download each package
 	# unzip them
 	# collect information about the package (list of files)
-
-	
-
-
-#    check_dist('pydigger')
-#    print(hi())
-
-#def hi():
-#    return "Hi from PyDigger"
-#
-#def check_dist(dist_name):
-#    try:
-#        bm = pkg_resources.get_distribution(dist_name)
-#    except pkg_resources.DistributionNotFound:
-#        print("Distribution '{}' not found".format(dist_name))
-#        return
-#
-#    print(bm.requires())
-#    print(bm.version)
 
